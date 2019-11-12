c84b39151b297c0d40ba28a2fbe85e52b0533f33
isaiahstarkey <54735039+isaiahstarkey@users.noreply.github.com>
2019-11-11 21:22:15 -1000

    updated names on landing page and navbar

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -7,13 +7,9 @@ class Landing extends React.Component {
     return (
         <Grid verticalAlign='middle' textAlign='center' container>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
           <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
+            <h1>UH Manoa Marketplace</h1>
+            <p>A marketplace for UH Manoa students, faculty, and staff.</p>
           </Grid.Column>
 
         </Grid>
