4b2fe1b6a2e657018b420d10b91cf9f4fa22b07e
isaiahstarkey <54735039+isaiahstarkey@users.noreply.github.com>
2019-11-13 10:50:45 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,14 +6,23 @@ class Landing extends React.Component {
   render() {
     return (
         <Grid verticalAlign='middle' textAlign='center' container>
-          <Grid.Column width={8}>
+          <Grid.Column>
             <h1>A marketplace for UH Manoa students, faculty, and staff.</h1>
             <List>
               <List.Item>
-
+                <h1>Buy</h1>
+                <p>items listed from other UH Manoa students, faculty, and staff</p>
+              </List.Item>
+              <List.Item>
+                <h1>Sell</h1>
+                <p>your old UH Manoa-related items too others how may need it</p>
+              </List.Item>
+              <List.Item>
+                <h1>Trade</h1>
+                <p>your items for other items you may want</p>
               </List.Item>
             </List>
-          </Grid.Column>
+            </Grid.Column>
         </Grid>
     );
   }
