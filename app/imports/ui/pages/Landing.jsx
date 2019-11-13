4b5e7dd32d9859141b77e4a042034d3ace4287ea
isaiahstarkey <54735039+isaiahstarkey@users.noreply.github.com>
2019-11-13 10:35:56 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, List } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
         <Grid verticalAlign='middle' textAlign='center' container>
-
           <Grid.Column width={8}>
-            <h1>UH Manoa Marketplace</h1>
-            <p>A marketplace for UH Manoa students, faculty, and staff.</p>
-          </Grid.Column>
+            <h1>A marketplace for UH Manoa students, faculty, and staff.</h1>
+            <List>
+              <List.Item>
 
+              </List.Item>
+            </List>
+          </Grid.Column>
         </Grid>
     );
   }
