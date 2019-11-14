d78a0a37bc39a1352d5ed7fc99f0466f8bef7ca6
isaiahstarkey <54735039+isaiahstarkey@users.noreply.github.com>
2019-11-14 08:28:00 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import { Container, Grid, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-          <Grid.Column>
-            <h1>A marketplace for UH Manoa students, faculty, and staff.</h1>
-            <List>
-              <List.Item>
-                <h1>Buy</h1>
-                <p>items listed from other UH Manoa students, faculty, and staff</p>
-              </List.Item>
-              <List.Item>
-                <h1>Sell</h1>
-                <p>your old UH Manoa-related items too others how may need it</p>
-              </List.Item>
-              <List.Item>
-                <h1>Trade</h1>
-                <p>your items for other items you may want</p>
-              </List.Item>
-            </List>
+        <Container borderless>
+          <p>
+            The UH Manoa Marketplace is a web application where people apart of the UH community are
+            able to buy or sell their personal belongings. Great for students who graduate and need to get
+            rid of their unwanted stuff once they are finished dorming at the Univsersity of Hawai’i at Manoa.
+          </p>
+          <Grid container stackable columns="3">
+            <Grid.Column>
+              <Header>
+                List
+              </Header>
             </Grid.Column>
-        </Grid>
+            <Grid.Column>
+              <Header>
+                Find
+              </Header>
+            </Grid.Column>
+            <Grid.Column>
+              <Header>
+                Meet
+              </Header>
+            </Grid.Column>
+          </Grid>
+        </Container>
     );
   }
 }
