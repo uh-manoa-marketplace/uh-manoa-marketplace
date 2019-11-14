9724f5eb39600cf80162de454d83a5cd3b7db7d1
isaiahstarkey <54735039+isaiahstarkey@users.noreply.github.com>
2019-11-14 10:02:26 -1000

    landingpage  M1

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,31 +1,49 @@
 import React from 'react';
-import { Container, Grid, Header } from 'semantic-ui-react';
+import { Container, Grid, Header, Icon, Button, List } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
         <Container borderless>
-          <p>
-            The UH Manoa Marketplace is a web application where people apart of the UH community are
-            able to buy or sell their personal belongings. Great for students who graduate and need to get
-            rid of their unwanted stuff once they are finished dorming at the Univsersity of Hawai’i at Manoa.
-          </p>
-          <Grid container stackable columns="3">
-            <Grid.Column>
-              <Header>
-                List
+          <Grid verticalAlign='middle' columns="2" container>
+            <Grid.Column width={8} textAlign='center'>
+              <Header as="h1">
+                Buy, Sell, Trade with others in the UH community
               </Header>
+              <List>
+                <List.Item>
+                  <Header as="h3"><Icon name="list alternate outline" size="large"/></Header>
+                  <p>
+                    <span className='headings'>List</span> UH Manoa-related items that you do not use
+                    anymore but others in the UH community may want or need.
+                  </p>
+                </List.Item>
+                <List.Item>
+                  <Header as="h3">
+                    <Icon name="search" size="large"/>
+                  </Header>
+                  <p>
+                    <span className='headings'>Find</span> UH Manoa-related items you may want or need for cheaper prices.
+                  </p>
+                </List.Item>
+                <List.Item>
+                  <Header as="h3">
+                    <Icon name="handshake outline" size="large"/>
+                  </Header>
+                  <p>
+                    <span className='headings'>Meet</span> buyers and sellers on UH Manoa
+                    campus for safe and convenient transactions.
+                  </p>
+                </List.Item>
+              </List>
             </Grid.Column>
-            <Grid.Column>
-              <Header>
-                Find
+            <Grid.Column textAlign='center'>
+              <Header as="h1">
+                Start listing today!
               </Header>
-            </Grid.Column>
-            <Grid.Column>
-              <Header>
-                Meet
-              </Header>
+              <Button as={NavLink} exact to="/signup">Signup</Button>
             </Grid.Column>
           </Grid>
         </Container>
