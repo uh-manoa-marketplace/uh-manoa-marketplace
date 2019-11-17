a903ffe2d766ccc4937578f197da05bf24b0573d
Kiara Eltagonde <kiarae@hawaii.edu>
2019-11-17 01:42:24 -1000

    Fixing Issues

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -7,13 +7,9 @@ import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
-<<<<<<< HEAD
-import Selling from '../pages/Selling';
 import ListStuffAdmin from '../pages/ListStuffAdmin';
-=======
 import ItemsPage from '../pages/ItemsPage';
 import ItemsPageAdmin from '../pages/ItemsPageAdmin';
->>>>>>> issue-007
 import AddStuff from '../pages/AddStuff';
 import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
@@ -32,11 +28,7 @@ class App extends React.Component {
               <Route exact path="/" component={Landing}/>
               <Route path="/signin" component={Signin}/>
               <Route path="/signup" component={Signup}/>
-<<<<<<< HEAD
-              <ProtectedRoute path="/list" component={Selling}/>
-=======
               <ProtectedRoute path="/list" component={ItemsPage}/>
->>>>>>> issue-007
               <ProtectedRoute path="/add" component={AddStuff}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
               <AdminProtectedRoute path="/admin" component={ItemsPageAdmin}/>
