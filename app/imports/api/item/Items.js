c93cb75cf26d4ddce0fe1b30f461620c731c4e0b
Deshay Clemons <dclemons@hawaii.edu>
2019-11-16 16:15:41 -1000

    Completed issue-007

---
+++ app/imports/api/item/Items.js
@@ -0,0 +1,26 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/** Define a Mongo collection to hold the data. */
+const Items = new Mongo.Collection('Items');
+
+/** Define a schema to specify the structure of each document in the collection. */
+const ItemsSchema = new SimpleSchema({
+  name: String,
+  price: Number,
+  image: String,
+  owner: String,
+  condition: {
+    type: String,
+    allowedValues: ['excellent', 'good', 'fair', 'poor'],
+    defaultValue: 'good',
+  },
+  description: String,
+}, { tracker: Tracker });
+
+/** Attach this schema to the collection. */
+Items.attachSchema(ItemsSchema);
+
+/** Make the collection and schema available to other code. */
+export { Items, ItemsSchema };
--- app/imports/startup/server/Publications.js
+++ app/imports/startup/server/Publications.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
 import { Stuffs } from '../../api/stuff/Stuff';
+import { Items } from '../../api/item/Items';
 
 /** This subscription publishes only the documents associated with the logged in user */
 Meteor.publish('Stuff', function publish() {
@@ -15,6 +16,23 @@ Meteor.publish('Stuff', function publish() {
 Meteor.publish('StuffAdmin', function publish() {
   if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
     return Stuffs.find();
+  }
+  return this.ready();
+});
+
+
+Meteor.publish('Items', function publish() {
+  if (this.userId) {
+    const username = Meteor.users.findOne(this.userId).username;
+    return Items.find({ owner: username });
+  }
+  return this.ready();
+});
+
+/** This subscription publishes all documents regardless of user, but only if the logged in user is the Admin. */
+Meteor.publish('ItemsAdmin', function publish() {
+  if (this.userId && Roles.userIsInRole(this.userId, 'admin')) {
+    return Items.find();
   }
   return this.ready();
 });
