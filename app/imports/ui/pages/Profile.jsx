048819df05bd870aac5306eaa2b4f33a21743f20
vjodar <vjodar@hawaii.edu>
2019-11-19 18:55:50 -1000

    Updated Profile page to use cards
    Consolidated profile into a card. Created seperate section for card group to represent the user's favorited items

--- app/imports/ui/pages/Profile.jsx
+++ app/imports/ui/pages/Profile.jsx
@@ -1,25 +1,99 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Image, Icon, Card, Grid, Header } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Profile extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
-
-          <Grid.Column width={8}>
-            <h1>Profile Page</h1>
-            <Image src={'https://pmcdeadline2.files.wordpress.com/2018/09/6241.jpeg?w=605'}/>
-            <p>
-              Place profile info here. <br/>
-              Name <br/>
-              Title <br/>
-              Major <br/>
-              Contact info <br/>
-              Etc. <br/>
-            </p>
+        <Grid columns={2} divided container>
+          <Grid.Column>
+            <Card fluid color={'black'}>
+              <Image src='https://pmcdeadline2.files.wordpress.com/2018/09/6241.jpeg?w=605' wrapped ui={false} />
+              <Card.Content>
+                <Card.Header>Johnathan Foobar III</Card.Header>
+                <Card.Meta>Joined in 2016</Card.Meta>
+                <Card.Description>
+                  Johnathan is a junior majoring in ICS.
+                </Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name='user' />
+                  10 Friends
+                </a>
+              </Card.Content>
+            </Card>
           </Grid.Column>
 
+          <Grid.Column>
+            <Header as={'h2'} textAlign={'center'}>Favorites</Header>
+            <Card.Group centered itemsPerRow={2}>
+              <Card fluid color={'black'}>
+                <Image src='https://pmcdeadline2.files.wordpress.com/2018/09/6241.jpeg?w=605' wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>Johnathan Foobar III</Card.Header>
+                  <Card.Meta>Joined in 2016</Card.Meta>
+                  <Card.Description>
+                    Johnathan is a junior majoring in ICS.
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='user' />
+                    10 Friends
+                  </a>
+                </Card.Content>
+              </Card>
+              <Card fluid color={'black'}>
+                <Image src='https://pmcdeadline2.files.wordpress.com/2018/09/6241.jpeg?w=605' wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>Johnathan Foobar III</Card.Header>
+                  <Card.Meta>Joined in 2016</Card.Meta>
+                  <Card.Description>
+                    Johnathan is a junior majoring in ICS.
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='user' />
+                    10 Friends
+                  </a>
+                </Card.Content>
+              </Card>
+              <Card fluid color={'black'}>
+                <Image src='https://pmcdeadline2.files.wordpress.com/2018/09/6241.jpeg?w=605' wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>Johnathan Foobar III</Card.Header>
+                  <Card.Meta>Joined in 2016</Card.Meta>
+                  <Card.Description>
+                    Johnathan is a junior majoring in ICS.
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='user' />
+                    10 Friends
+                  </a>
+                </Card.Content>
+              </Card>
+              <Card fluid color={'black'}>
+                <Image src='https://pmcdeadline2.files.wordpress.com/2018/09/6241.jpeg?w=605' wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>Johnathan Foobar III</Card.Header>
+                  <Card.Meta>Joined in 2016</Card.Meta>
+                  <Card.Description>
+                    Johnathan is a junior majoring in ICS.
+                  </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <a>
+                    <Icon name='user' />
+                    10 Friends
+                  </a>
+                </Card.Content>
+              </Card>
+            </Card.Group>
+          </Grid.Column>
         </Grid>
     );
   }
