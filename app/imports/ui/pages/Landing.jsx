e128655018042590eb305fc7500b606651b6034e
Eric Rivera <emr6info@gmail.com>
2019-12-04 17:47:56 -1000

    Update Landing.jsx

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -3,31 +3,23 @@ import React, { Component } from 'react';
 import {
   Button,
   Container,
-  Divider,
   Grid,
   Header,
   Icon,
   Image,
-  List,
   Menu,
   Responsive,
   Segment,
   Sidebar,
 } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
 
-// Heads up!
-// We using React Static to prerender our docs with server side rendering, this is a quite simple solution.
-// For more advanced usage please check Responsive docs under the "Usage" section.
 const getWidth = () => {
   const isSSR = typeof window === 'undefined';
 
   return isSSR ? Responsive.onlyTablet.minWidth : Responsive.onlyTablet.minWidth;
 };
 
-/* eslint-disable react/no-multi-comp */
-/* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
- * such things.
- */
 const HomepageHeading = ({ mobile }) => (
     <Container text>
       <Header
@@ -51,7 +43,7 @@ const HomepageHeading = ({ mobile }) => (
             marginTop: mobile ? '0.5em' : '1.5em',
           }}
       />
-      <Button primary size='huge'>
+      <Button as={NavLink} primary size='huge' exact to="/signup">
         Get Started
         <Icon name='right arrow' />
       </Button>
@@ -205,7 +197,8 @@ const HomepageLayout = () => (
               </p>
             </Grid.Column>
             <Grid.Column floated='right' width={6}>
-              <Image bordered rounded size='large' src='/images/wireframe/white-image.png' />
+              <Image bordered rounded size='large'
+                     src='https://manoa.hawaii.edu/wp/wp-content/uploads/2017/09/campus-manoa-valley.jpg' />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row>
@@ -214,95 +207,6 @@ const HomepageLayout = () => (
             </Grid.Column>
           </Grid.Row>
         </Grid>
-      </Segment>
-      <Segment style={{ padding: '0em' }} vertical>
-        <Grid celled='internally' columns='equal' stackable>
-          <Grid.Row textAlign='center'>
-            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-              <Header as='h3' style={{ fontSize: '2em' }}>
-                &quot;What a Company&quot;
-              </Header>
-              <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
-            </Grid.Column>
-            <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-              <Header as='h3' style={{ fontSize: '2em' }}>
-                &quot;I shouldn&apos;t have gone with their competitor.&quot;
-              </Header>
-              <p style={{ fontSize: '1.33em' }}>
-                <Image avatar src='/images/avatar/large/nan.jpg' />
-                <b>Nan</b> Chief Fun Officer Acme Toys
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment style={{ padding: '8em 0em' }} vertical>
-        <Container text>
-          <Header as='h3' style={{ fontSize: '2em' }}>
-            Breaking The Grid, Grabs Your Attention
-          </Header>
-          <p style={{ fontSize: '1.33em' }}>
-            Instead of focusing on content creation and hard work, we have learned how to master the
-            art of doing nothing by providing massive amounts of whitespace and generic content that
-            can seem massive, monolithic and worth your attention.
-          </p>
-          <Button as='a' size='large'>
-            Read More
-          </Button>
-          <Divider
-              as='h4'
-              className='header'
-              horizontal
-              style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-          >
-            <a href='#'>Case Studies</a>
-          </Divider>
-          <Header as='h3' style={{ fontSize: '2em' }}>
-            Did We Tell You About Our Bananas?
-          </Header>
-          <p style={{ fontSize: '1.33em' }}>
-            Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
-            it is really true. It took years of gene splicing and combinatory DNA research, but our
-            bananas can really dance.
-          </p>
-          <Button as='a' size='large'>
-            I amStill Quite Interested
-          </Button>
-        </Container>
-      </Segment>
-      <Segment inverted vertical style={{ padding: '5em 0em' }}>
-        <Container>
-          <Grid divided inverted stackable>
-            <Grid.Row>
-              <Grid.Column width={3}>
-                <Header inverted as='h4' content='About' />
-                <List link inverted>
-                  <List.Item as='a'>Sitemap</List.Item>
-                  <List.Item as='a'>Contact Us</List.Item>
-                  <List.Item as='a'>Religious Ceremonies</List.Item>
-                  <List.Item as='a'>Gazebo Plans</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={3}>
-                <Header inverted as='h4' content='Services' />
-                <List link inverted>
-                  <List.Item as='a'>Banana Pre-Order</List.Item>
-                  <List.Item as='a'>DNA FAQ</List.Item>
-                  <List.Item as='a'>How To Access</List.Item>
-                  <List.Item as='a'>Favorite X-Men</List.Item>
-                </List>
-              </Grid.Column>
-              <Grid.Column width={7}>
-                <Header as='h4' inverted>
-                  Footer Header
-                </Header>
-                <p>
-                  Extra space for a call to action inside the footer that could help re-engage users.
-                </p>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Container>
       </Segment>
     </ResponsiveContainer>
 );
