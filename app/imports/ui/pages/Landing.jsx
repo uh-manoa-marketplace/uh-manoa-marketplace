e8c428bbd3186f4b8797ec1e3988af6312f143a3
Kiara Eltagonde <kiarae@hawaii.edu>
2019-12-09 22:42:16 -1000

    Initial

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,208 +1,46 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import {
-  Button,
-  Container,
-  Grid,
-  Header,
-  Icon,
-  Image,
-  Menu,
-  Responsive,
-  Segment,
-  Sidebar,
-} from 'semantic-ui-react';
+import React from 'react';
+import { Grid, Icon, Header, Button } from 'semantic-ui-react';
 import { NavLink } from 'react-router-dom';
 
-const getWidth = () => {
-  const isSSR = typeof window === 'undefined';
-
-  return isSSR ? Responsive.onlyTablet.minWidth : Responsive.onlyTablet.minWidth;
-};
-
-const HomepageHeading = ({ mobile }) => (
-    <Container text>
-      <Header
-          as='h1'
-          content='UH Manoa Marketplace'
-          inverted
-          style={{
-            fontSize: mobile ? '2em' : '4em',
-            fontWeight: 'normal',
-            marginBottom: 0,
-            marginTop: mobile ? '1.5em' : '3em',
-          }}
-      />
-      <Header
-          as='h2'
-          content='Buy, Sell, & Trade with others in the UH community'
-          inverted
-          style={{
-            fontSize: mobile ? '1.5em' : '1.7em',
-            fontWeight: 'normal',
-            marginTop: mobile ? '0.5em' : '1.5em',
-          }}
-      />
-      <Button as={NavLink} primary size='huge' exact to="/signup">
-        Get Started
-        <Icon name='right arrow' />
-      </Button>
-    </Container>
-);
-
-HomepageHeading.propTypes = {
-  mobile: PropTypes.bool,
-};
-
-/* Heads up!
- * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
- * It can be more complicated, but you can create really flexible markup.
- */
-class DesktopContainer extends Component {
-  state = {}
-
-  hideFixedMenu = () => this.setState({ fixed: false })
-
-  showFixedMenu = () => this.setState({ fixed: true })
-
+/** A simple static component to render some text for the landing page. */
+class Landing extends React.Component {
   render() {
-    const { children } = this.props;
-
     return (
-        <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
-          <Segment
-              inverted
-              textAlign='center'
-              style={{ minHeight: 700, padding: '1em 0em' }}
-              vertical
-          >
-            <HomepageHeading />
-          </Segment>
-
-          {children}
-        </Responsive>
+        <div className='landing'>
+          <Grid container
+                centered
+                stackable>
+            <Grid.Column textAlign='center'>
+              <Header as='h1'
+                      inverted
+                      style={{
+                        fontSize: '4em',
+                        fontWeight: 'normal',
+                        marginBottom: 0,
+                        marginTop: '3em',
+                      }}>
+                UH Manoa Marketplace
+              </Header>
+              <Header as='h2'
+                      inverted
+                      style={{
+                        fontSize: '1.7em',
+                        fontWeight: 'normal',
+                        marginTop: '1.5em',
+                      }}>
+                Buy, Sell, & Trade with others in the UH community
+              </Header>
+              <Button as={NavLink}
+                      primary
+                      size='huge'
+                      exact
+                      to="/signup">Get Started
+                <Icon name='right arrow'/></Button>
+            </Grid.Column>
+          </Grid>
+        </div>
     );
   }
 }
 
-DesktopContainer.propTypes = {
-  children: PropTypes.node,
-};
-
-class MobileContainer extends Component {
-  state = {}
-
-  handleSidebarHide = () => this.setState({ sidebarOpened: false })
-
-  handleToggle = () => this.setState({ sidebarOpened: true })
-
-  render() {
-    const { children } = this.props;
-    const { sidebarOpened } = this.state;
-
-    return (
-        <Responsive
-            as={Sidebar.Pushable}
-            getWidth={getWidth}
-            maxWidth={Responsive.onlyMobile.maxWidth}
-        >
-          <Sidebar
-              as={Menu}
-              animation='push'
-              inverted
-              onHide={this.handleSidebarHide}
-              vertical
-              visible={sidebarOpened}
-          >
-            <Menu.Item as='a' active>
-              Home
-            </Menu.Item>
-            <Menu.Item as='a'>Work</Menu.Item>
-            <Menu.Item as='a'>Company</Menu.Item>
-            <Menu.Item as='a'>Careers</Menu.Item>
-            <Menu.Item as='a'>Log in</Menu.Item>
-            <Menu.Item as='a'>Sign Up</Menu.Item>
-          </Sidebar>
-
-          <Sidebar.Pusher dimmed={sidebarOpened}>
-            <Segment
-                inverted
-                textAlign='center'
-                style={{ minHeight: 350, padding: '1em 0em' }}
-                vertical
-            >
-              <Container>
-                <Menu inverted pointing secondary size='large'>
-                  <Menu.Item onClick={this.handleToggle}>
-                    <Icon name='sidebar' />
-                  </Menu.Item>
-                  <Menu.Item position='right'>
-                    <Button as='a' inverted>
-                      Log in
-                    </Button>
-                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-                      Sign Up
-                    </Button>
-                  </Menu.Item>
-                </Menu>
-              </Container>
-              <HomepageHeading mobile />
-            </Segment>
-
-            {children}
-          </Sidebar.Pusher>
-        </Responsive>
-    );
-  }
-}
-
-MobileContainer.propTypes = {
-  children: PropTypes.node,
-};
-
-const ResponsiveContainer = ({ children }) => (
-    <div>
-      <DesktopContainer>{children}</DesktopContainer>
-      <MobileContainer>{children}</MobileContainer>
-    </div>
-);
-
-ResponsiveContainer.propTypes = {
-  children: PropTypes.node,
-};
-
-const HomepageLayout = () => (
-    <ResponsiveContainer>
-      <Segment style={{ padding: '8em 0em' }} vertical>
-        <Grid container stackable verticalAlign='middle'>
-          <Grid.Row>
-            <Grid.Column width={8}>
-              <Header as='h3' style={{ fontSize: '2em' }}>
-                List
-              </Header>
-              <p style={{ fontSize: '1.33em' }}>
-                UH Manoa-related items that you do not use anymore but others in the UH community may want or need.
-              </p>
-              <Header as='h3' style={{ fontSize: '2em' }}>
-                Find
-              </Header>
-              <p style={{ fontSize: '1.33em' }}>
-                UH Manoa-related items you may want or need for cheaper prices.
-              </p>
-              <Header as='h3' style={{ fontSize: '2em' }}>
-                Meet
-              </Header>
-              <p style={{ fontSize: '1.33em' }}>
-                buyers and sellers on UH Manoa campus for safe and convenient transactions.
-              </p>
-            </Grid.Column>
-            <Grid.Column floated='right' width={6}>
-              <Image bordered rounded size='large'
-                     src='https://manoa.hawaii.edu/wp/wp-content/uploads/2017/09/campus-manoa-valley.jpg' />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-    </ResponsiveContainer>
-);
-export default HomepageLayout;
+export default Landing;
